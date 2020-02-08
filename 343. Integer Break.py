88cec3ae3e6e50738177decc8d00c606e8a3c13c
LiaoJJ <519312579@qq.com>
2020-02-08 18:44:58 -0500

    343

--- 343. Integer Break.py
+++ 343. Integer Break.py
@@ -1,1 +1,13 @@
-asds
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n<=2: return 1
+        if n<=3: return 2
+        dp = [0]*(n+1)
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 3
+        
+        for i in range(4, n+1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], dp[j] * dp[i-j])
+        return dp[n]
